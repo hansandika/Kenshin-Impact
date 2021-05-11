c6e83a575569f6dd1a8ca66b761ff85934d9ad12
Hans <hansandika70@gmail.com>
2021-05-12 03:24:40 +0700

    third push

---
+++ javascript/main.js
@@ -0,0 +1,19 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector(".menu");
+    const navLink = document.querySelectorAll('.menu-content');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('menu-active');   
+        navLink.forEach((e,index) => {
+            if(e.style.animation){
+                e.style.animation = "";
+            }else{
+                e.style.animation = `navLinkFade 0.5s ease forwards ${index /7 + 0.5}s`
+            }
+        })
+        burger.classList.toggle('toggle');
+    })
+}
+
+navSlide();
