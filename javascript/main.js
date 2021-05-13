3c1af2ccfd8af28572076f0b934af6e9b954e08d
Hans <hansandika70@gmail.com>
2021-05-14 05:21:41 +0700

    commit

--- javascript/main.js
+++ javascript/main.js
@@ -2,17 +2,28 @@ const navSlide = () => {
     const burger = document.querySelector('.burger');
     const sidebar = document.querySelector(".sidebar");
     const navLink = document.querySelectorAll('.sidebar-content');
-
+    const sidebarDiv = document.querySelector('.sidebar-div');
     burger.addEventListener('click', () => {
-        sidebar.classList.toggle('menu-active');   
+        sidebarDiv.style.display = "block";
+        if(sidebar.classList.contains('menu-active')){
+            sidebar.classList.remove('menu-active');
+        }
+        else{
+            sidebar.classList.add('menu-active');
+        } 
         navLink.forEach((e,index) => {
             if(e.style.animation){
                 e.style.animation = "";
             }else{
-                e.style.animation = `navLinkFade 0.5s ease forwards ${index /7 + 0.5}s`
+                e.style.animation = `navLinkFade 0.5s ease forwards ${index /7 + 0.25}s`
             }
         })
-        burger.classList.toggle('toggle');
+        if(burger.classList.contains('toggle')){
+            burger.classList.remove('toggle');
+            sidebarDiv.style.display = "none";
+        }else{
+            burger.classList.add('toggle');
+        }
     })
 }
 
@@ -24,16 +35,26 @@ document.addEventListener('scroll',() => {
         if(window.scrollY > 0){
             navbarLogo.style.visibility = 'visible';
             mainLogo.style.visibility = "hidden";
-            navbar.style.backgroundColor = "rgb(0, 0, 0, 0.8)";
+            navbar.style.backgroundColor = "rgba(0, 0, 0, 0.4)";
             // console.log(window.scrollY)
         }
         else if(window.scrollY == 0){
             navbarLogo.style.visibility = 'hidden';
             mainLogo.style.visibility = 'visible';
-            navbar.style.backgroundColor = "rgb(0, 0, 0, 0)";
+            navbar.style.backgroundColor = "rgba(0, 0, 0, 0)";
         }
     }
 })
 
-
+window.addEventListener('click', (e) =>{
+    const sidebarDiv = document.querySelector('.sidebar-div');
+    const sidebar = document.querySelector(".sidebar");
+    const burger = document.querySelector('.burger');
+    console.log("test");
+    if(e.target == sidebarDiv && sidebarDiv.style.display == "block"){
+        sidebarDiv.style.display = "none";
+        sidebar.classList.remove('menu-active');
+        burger.classList.remove('toggle');
+    }
+})
 navSlide();
