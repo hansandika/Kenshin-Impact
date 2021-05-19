9cd8bfee35132e97eba45272a3e7891840665ec3
Hans <hansandika70@gmail.com>
2021-05-20 01:59:17 +0700

    commit

--- javascript/character.js
+++ javascript/character.js
@@ -1,11 +1,11 @@
-const background = document.querySelector('.character-background');
-var flag = true;
+// const background = document.querySelector('.character-background');
+// var flag = true;
 
-setInterval(()=>{
-    if(flag){
-        background.style.backgroundImage = "url('../assets/background/Li Yue.jpg')";
-    }else{  
-        background.style.backgroundImage = "url('../assets/background/Mondstadt.jpg')";
-    }
-    flag = !flag;
-},15000)
+// setInterval(()=>{
+//     if(flag){
+//         background.style.backgroundImage = "url('../assets/background/Li Yue.jpg')";
+//     }else{  
+//         background.style.backgroundImage = "url('../assets/background/Mondstadt.jpg')";
+//     }
+//     flag = !flag;
+// },15000)
