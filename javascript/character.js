437281bc4a2ee83a219497698f869ba56a713ede
Hans <hansandika70@gmail.com>
2021-06-05 04:15:38 +0700

    commit

--- javascript/character.js
+++ javascript/character.js
@@ -146,18 +146,6 @@ const map = () =>{
     })
 }
 
-window.addEventListener("resize", () => {
-    const navbarLogo = document.querySelector("#logo-navbar");
-    const navbar = document.querySelector('nav');
-    if(screen.width >= 1025){
-        navbar.style.backgroundColor= "rgba(0, 0, 0, 0.6)";
-        navbarLogo.style.visibility = 'visible';
-    }else{
-        navbarLogo.style.visibility = 'visible';
-        navbar.style.backgroundColor = "rgba(0, 0, 0, 0.4)";  
-    }
-})
-
 const mapDropdown = () => {
     const dropdown = document.querySelector(".map-responsive");
     const arrow = document.querySelector('.arrow');
