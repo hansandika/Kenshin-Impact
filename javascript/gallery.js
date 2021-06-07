a36dc93d9f37ff7e60109b37172176a479bb65de
Hans <hansandika70@gmail.com>
2021-06-07 08:26:59 +0700

    commit

--- javascript/gallery.js
+++ javascript/gallery.js
@@ -1,33 +1,37 @@
 $(()=> {
-    let slideCount = $('.img-slider').length;
-    let slideWidth = $('.img-slider').width();
-    let totalwidth = slideCount * slideWidth;
+    const slideCount = $('.img-slider').length;
+    const slideWidth = $('.img-slider').width();
+    const totalwidth = slideCount * slideWidth;
     $('.slides').css({
         width: slideWidth,  
         marginLeft : -slideWidth
     });
     $('.img-slider:last-child').prependTo(".slides");
 
-    $('.left-slider').click(function () { 
+    $('.left-slider').click(() => { 
         $('.slides').animate({left: +slideWidth},500, ()=>{
             $('.img-slider:last-child').prependTo(".slides");
             $('.slides').css({left:0});
         })
     });
 
-    $('.right-slider').click(function () { 
+    $('.right-slider').click(() => { 
         $('.slides').animate({left: -slideWidth },500, ()=>{
             $('.img-slider:first-child').appendTo(".slides");
             $('.slides').css({left:0});
         })
     });
 
-
-    // setInterval(()=>[
-    //     $('.slides').animate({left: -slideWidth },500, ()=>{
-    //         $('.slide:first-child').appendTo(".slides");
-    //         $('.slides').css({left:0});
-    //     })
-    // ],1000);
 });
 
+$('.icon').click(() =>{
+    if($('.icon').hasClass('fa-moon')){
+        $('body').addClass('light-theme');
+        $('.icon').removeClass('fa-moon');
+        $('.icon').addClass('fa-sun');
+    }else{
+        $('body').removeClass('light-theme');
+        $('.icon').addClass('fa-moon');
+        $('.icon').removeClass('fa-sun');
+    }
+})
