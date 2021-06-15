9f7ea27bc74b4b08b2f010224e007b11903f5b21
Hans <hansandika70@gmail.com>
2021-06-15 19:38:18 +0700

    commit

--- javascript/gallery.js
+++ javascript/gallery.js
@@ -21,20 +21,20 @@ $(()=> {
         })
     });
 
+    $('.icon').click(() =>{
+        if($('.icon').hasClass('fa-moon')){
+            $('body').removeClass('light-theme');
+            $('.icon').removeClass('fa-moon');
+            $('.icon').addClass('fa-sun');
+            $('.left-slider img').attr('src','../assets/ICON/Arrow Left.png');
+            $('.right-slider img').attr('src','../assets/ICON/Arrow Right.png');
+        }else{
+            $('body').addClass('light-theme');
+            $('.icon').addClass('fa-moon');
+            $('.icon').removeClass('fa-sun');
+            $('.left-slider img').attr('src','../assets/ICON/BlackArrowLeft.png');
+            $('.right-slider img').attr('src','../assets/ICON/BlackArrowRight.png');
+        }
+    })
 });
 
-$('.icon').click(() =>{
-    if($('.icon').hasClass('fa-moon')){
-        $('body').removeClass('light-theme');
-        $('.icon').removeClass('fa-moon');
-        $('.icon').addClass('fa-sun');
-        $('.left-slider img').attr('src','../assets/ICON/Arrow Left.png');
-        $('.right-slider img').attr('src','../assets/ICON/Arrow Right.png');
-    }else{
-        $('body').addClass('light-theme');
-        $('.icon').addClass('fa-moon');
-        $('.icon').removeClass('fa-sun');
-        $('.left-slider img').attr('src','../assets/ICON/BlackArrowLeft.png');
-        $('.right-slider img').attr('src','../assets/ICON/BlackArrowRight.png');
-    }
-})
