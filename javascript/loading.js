44bf65cf023c17578ef20e07b6fef12661545c08
Hans <hansandika70@gmail.com>
2021-06-20 22:13:15 +0700

    commit

---
+++ javascript/loading.js
@@ -0,0 +1,12 @@
+const loaderWrapper = document.createElement('div');
+loaderWrapper.classList.add('loader-wrapper');
+const span = document.createElement('span');
+span.classList.add('loader');
+const span2 = document.createElement('span');
+span2.classList.add('loader-inner');
+span.appendChild(span2);
+loaderWrapper.appendChild(span);
+document.querySelector('body').prepend(loaderWrapper);
+$(document).ready(() => {
+    $(".loader-wrapper").fadeOut("slow");
+})
